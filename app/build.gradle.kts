2cb31da6155ada62b1218a7da63a43fa28182e7d
Vadim Yakshigulov <toadharvard@gmail.com>
2023-03-25 19:02:00 +0300

    fix: Bump kotlin version to fix grandle run task

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,10 +5,13 @@
  * For more details take a look at the 'Building Java & JVM projects' chapter in the Gradle
  * User Manual available at https://docs.gradle.org/7.2/userguide/building_java_projects.html
  */
+kotlin {
+    jvmToolchain(8)
+}
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.5.0"
+    id("org.jetbrains.kotlin.jvm") version "1.8.10"
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
@@ -36,5 +39,5 @@ dependencies {
 
 application {
     // Define the main class for the application.
-    mainClass.set("app:AppKt")
+    mainClass.set("app.AppKt")
 }
