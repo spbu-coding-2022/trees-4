1b502cc5599accaf421390fc3f76f43c234046bd
Vadim Yakshigulov <toadharvard@gmail.com>
2023-03-25 19:02:00 +0300

    fix: Bump kotlin version to fix grandle run task

--- binary-search-trees/build.gradle.kts
+++ binary-search-trees/build.gradle.kts
@@ -5,10 +5,13 @@
  * For more details take a look at the 'Building Java & JVM projects' chapter in the Gradle
  * User Manual available at https://docs.gradle.org/7.2/userguide/building_java_projects.html
  */
+kotlin {
+    jvmToolchain(8)
+}
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.5.0"
+    id("org.jetbrains.kotlin.jvm") version "1.8.10"
 }
 
 repositories {
