cf694286575d9c4c30a92daade84af0d38c95b9c
Vadim Yakshigulov <toadharvard@gmail.com>
2023-04-03 23:36:03 +0300

    fix: Remove `data` in nodes because of comparation stackoverflow

--- binary-search-trees/src/main/kotlin/bst/node/BSNode.kt
+++ binary-search-trees/src/main/kotlin/bst/node/BSNode.kt
@@ -1,6 +1,6 @@
 package bst.node
 
-internal data class BSNode<E : Comparable<E>>(
+internal class BSNode<E : Comparable<E>>(
     override var value: E,
     override var left: BinSearchTreeNode<E>? = null,
     override var right: BinSearchTreeNode<E>? = null,
