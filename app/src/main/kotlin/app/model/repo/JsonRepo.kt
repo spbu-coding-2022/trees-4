6bacc59d357ac0ba52dfbcbb44ad02e7a484c3dd
Vadim Yakshigulov <toadharvard@gmail.com>
2023-04-16 22:40:47 +0300

    feat: Add template for repositories

---
+++ app/src/main/kotlin/app/model/repo/JsonRepo.kt
@@ -0,0 +1,20 @@
+package app.model.repo
+
+import app.model.bst.BinarySearchTree
+import app.model.bst.node.BinTreeNode
+import app.model.repo.serialization.strategy.SerializationStrategy
+
+class JsonRepo<E : Comparable<E>,
+        Node : BinTreeNode<E, Node>,
+        BST : BinarySearchTree<E, Node>>(
+    strategy: SerializationStrategy<E, Node>
+) : Repository<E, Node, BST>(strategy) {
+    override fun save(tree: BST) {
+        TODO("Not yet implemented")
+    }
+
+    override fun load(factory: () -> BST): BST {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/kotlin/app/model/repo/serialization/Serializable.kt
@@ -0,0 +1,32 @@
+package app.model.repo.serialization
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+enum class TypeOfTree(val savingName: String) {
+    BINARY_SEARCH_TREE("BINARY_SEARCH_TREE"),
+    RED_BLACK_TREE("RED_BLACK_TREE"),
+    AVL_TREE("AVL_TREE")
+}
+
+@Serializable
+class SerializableNode(
+    val value: SerializableValue,
+    val metadata: Metadata,
+    val left: SerializableNode? = null,
+    val right: SerializableNode? = null,
+)
+
+@Serializable
+class SerializableTree(
+    val typeOfTree: TypeOfTree,
+    val root: SerializableNode?
+)
+
+@Serializable
+@JvmInline
+value class Metadata(val value: String)
+
+@Serializable
+@JvmInline
+value class SerializableValue(val value: String)
