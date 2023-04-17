7dd3c4e18ef98012538536dd7175c71ab88c8059
Efim Perevalov <es.perevalov@mail.ru>
2023-04-18 01:49:06 +0300

    test: Fix the problem with the immutable root in AVLBalancerTest

--- app/src/test/kotlin/app/model/bst/balancer/AVLBalancerTest.kt
+++ app/src/test/kotlin/app/model/bst/balancer/AVLBalancerTest.kt
@@ -1,6 +1,5 @@
 package app.model.bst.balancer
 
-import app.model.bst.node.AVLNode
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
@@ -9,13 +8,13 @@ class AVLBalancerTest {
     private val balancer = AVLBalancer<Int>()
     @Test
     fun `test method 'add' in avl tree balancer`() {
-        val root = AVLNode(5)
-        balancer.add(root, 3, true)
-        balancer.add(root, 7, true)
-        balancer.add(root, 2, true)
-        balancer.add(root, 4, true)
-        balancer.add(root, 6, true)
-        balancer.add(root, 8, true)
+        var root = balancer.add(null, 2, true)
+        root = balancer.add(root, 3, true)
+        root = balancer.add(root, 4, true)
+        root = balancer.add(root, 5, true)
+        root = balancer.add(root, 6, true)
+        root = balancer.add(root, 7, true)
+        root = balancer.add(root, 8, true)
 
         /*
              constructed tree:
@@ -31,19 +30,19 @@ class AVLBalancerTest {
         assertEquals(2, root.left?.left?.value)
         assertEquals(4, root.left?.right?.value)
         assertEquals(7, root.right?.value)
-        assertEquals(6, root.right?.left?.value)
         assertEquals(8, root.right?.right?.value)
+        assertEquals(6, root.right?.left?.value)
     }
 
     @Test
     fun `test method 'remove' in avl tree balancer`() {
-        val root = AVLNode(5)
-        balancer.add(root, 3, true)
-        balancer.add(root, 7, true)
-        balancer.add(root, 2, true)
-        balancer.add(root, 4, true)
-        balancer.add(root, 6, true)
-        balancer.add(root, 8, true)
+        var root = balancer.add(null, 5, true)
+        root = balancer.add(root, 3, true)
+        root = balancer.add(root, 7, true)
+        root = balancer.add(root, 2, true)
+        root = balancer.add(root, 4, true)
+        root = balancer.add(root, 6, true)
+        root = balancer.add(root, 8, true)
 
         /*
             constructed tree:
@@ -54,23 +53,41 @@ class AVLBalancerTest {
             2     4    6    8
          */
 
-        balancer.remove(root, 2)
+        balancer.remove(root, 5)
 
         /*
             constructed tree:
-                    5
+                    6
                  /      \
                3         7
-                 \      /  \
-                  4    6    8
+             /   \         \
+            2     4         8
          */
 
-        assertEquals(5, root.value)
+        assertEquals(6, root.value)
         assertEquals(3, root.left?.value)
-        assertNull(root.left?.left)
+        assertEquals(2, root.left?.left?.value)
         assertEquals(4, root.left?.right?.value)
         assertEquals(7, root.right?.value)
-        assertEquals(6, root.right?.left?.value)
+        assertNull(root.right?.left?.value)
+        assertEquals(8, root.right?.right?.value)
+
+        balancer.remove(root, 3)
+
+        /*
+            constructed tree:
+                    6
+                 /      \
+               4         7
+             /             \
+            2               8
+         */
+
+        assertEquals(6, root.value)
+        assertEquals(4, root.left?.value)
+        assertEquals(2, root.left?.left?.value)
+        assertNull(root.left?.right?.value)
+        assertEquals(7, root.right?.value)
         assertEquals(8, root.right?.right?.value)
     }
 }
