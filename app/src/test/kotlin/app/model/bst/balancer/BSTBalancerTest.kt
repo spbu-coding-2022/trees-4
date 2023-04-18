dcecdcb45b3b455822a2bc446082a2e510506404
Дьячков Виталий <86203628+YazoonDinalt@users.noreply.github.com>
2023-04-18 14:13:55 +0300

    test: Add test balance rb and bs tree

--- app/src/test/kotlin/app/model/bst/balancer/BSTBalancerTest.kt
+++ app/src/test/kotlin/app/model/bst/balancer/BSTBalancerTest.kt
@@ -7,17 +7,16 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class BSTBalancerTest {
-
+    private val balancer = BSBalancer<Int>()
     @Test
     fun `test method 'add' in bs tree`() {
-        val tree = BSTree<Int>()
+        var root = balancer.add(null, 5, true)
 
-        tree.add(5)
-        tree.add(1)
-        tree.add(7)
-        tree.add(4)
-        tree.add(6)
-        tree.add(3)
+        root = balancer.add(root, 1, true)
+        root = balancer.add(root , 7, true)
+        root = balancer.add(root,4, true )
+        root = balancer.add(root , 6, true)
+        root = balancer.add(root,3, true )
 
         /*
              constructed tree:
@@ -30,24 +29,24 @@ class BSTBalancerTest {
                3
          */
 
-        assertEquals(5, tree.root?.value)
-        assertEquals(1, tree.root?.left?.value)
-        assertEquals(4, tree.root?.left?.right?.value)
-        assertEquals(3, tree.root?.left?.right?.left?.value)
-        assertEquals(7, tree.root?.right?.value)
-        assertEquals(6, tree.root?.right?.left?.value)
+        assertEquals(5, root.value)
+        assertEquals(1, root.left?.value)
+        assertEquals(4, root.left?.right?.value)
+        assertEquals(3, root.left?.right?.left?.value)
+        assertEquals(7, root.right?.value)
+        assertEquals(6, root.right?.left?.value)
     }
 
     @Test
     fun `test method 'remove' in bs tree`() {
-        val tree = BSTree<Int>()
+        var root = balancer.add(null, 5, true)
 
-        tree.add(5)
-        tree.add(1)
-        tree.add(7)
-        tree.add(4)
-        tree.add(6)
-        tree.add(3)
+        root = balancer.add(root, 1, true)
+        root = balancer.add(root , 7, true)
+        root = balancer.add(root,4, true )
+        root = balancer.add(root , 6, true)
+        root = balancer.add(root,3, true )
+
 
         /*
              constructed tree:
@@ -60,7 +59,7 @@ class BSTBalancerTest {
                3
          */
 
-        tree.remove(5)
+        root = balancer.remove(root,5)!!
 
         /*
              constructed tree:
@@ -74,10 +73,10 @@ class BSTBalancerTest {
          */
 
 
-        assertEquals(6, tree.root?.value)
-        assertEquals(1, tree.root?.left?.value)
-        assertEquals(4, tree.root?.left?.right?.value)
-        assertEquals(7, tree.root?.right?.value)
-        assertEquals(3, tree.root?.left?.right?.left?.value)
+        assertEquals(6, root.value)
+        assertEquals(1, root.left?.value)
+        assertEquals(4, root.left?.right?.value)
+        assertEquals(7, root.right?.value)
+        assertEquals(3, root.left?.right?.left?.value)
     }
 }
--- app/src/test/kotlin/app/model/bst/balancer/RBBalancerTest.kt
+++ app/src/test/kotlin/app/model/bst/balancer/RBBalancerTest.kt
@@ -6,16 +6,15 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class RBBalancerTest {
-
+    private val balancer = RBBalancer<Int>()
 
     @Test
     fun `test method 'add' in rb tree`() {
-        val tree = RBTree<Int>()
+        var root = balancer.add(null, 1, true)
 
-        tree.add(1)
-        tree.add(2 )
-        tree.add(3 )
-        tree.add(4 )
+        root = balancer.add(root, 2, true)
+        root = balancer.add(root , 3, true)
+        root = balancer.add(root,4, true )
 
 
         /*
@@ -28,27 +27,24 @@ class RBBalancerTest {
                  */
 
 
-        assertEquals(2, tree.root?.value)
-        assertEquals(1, tree.root?.left?.value)
-        assertEquals(3, tree.root?.right?.value)
-        assertEquals(4, tree.root?.right?.right?.value)
-        assertEquals(RedBlackTreeNode.Color.BLACK, tree.root?.color)
-        assertEquals(RedBlackTreeNode.Color.BLACK, tree.root?.left?.color)
-        assertEquals(RedBlackTreeNode.Color.BLACK, tree.root?.right?.color)
-        assertEquals(RedBlackTreeNode.Color.RED, tree.root?.right?.right?.color)
+        assertEquals(2, root.value)
+        assertEquals(1, root.left?.value)
+        assertEquals(3, root.right?.value)
+        assertEquals(4, root.right?.right?.value)
+        assertEquals(RedBlackTreeNode.Color.BLACK, root.color)
+        assertEquals(RedBlackTreeNode.Color.BLACK, root.left?.color)
+        assertEquals(RedBlackTreeNode.Color.BLACK, root.right?.color)
+        assertEquals(RedBlackTreeNode.Color.RED, root.right?.right?.color)
 
     }
 
     @Test
     fun `test method 'remove' in rb tree`() {
-        val tree = RBTree<Int>()
+        var root = balancer.add(null, 1, true)
 
-        tree.add(1)
-        tree.add(2)
-        tree.add(3)
-        tree.add(4)
-
-
+        root = balancer.add(root, 2, true)
+        root = balancer.add(root , 3, true)
+        root = balancer.add(root,4, true )
         /*
                      constructed tree:
                            2(BLACK)
@@ -59,7 +55,7 @@ class RBBalancerTest {
                  */
 
 
-        tree.remove( 2)
+        root = balancer.remove(root,2)!!
 
 
         /*
@@ -70,12 +66,12 @@ class RBBalancerTest {
                  */
 
 
-        assertEquals(3, tree.root?.value)
-        assertEquals(1, tree.root?.left?.value)
-        assertEquals(4, tree.root?.right?.value)
-        assertEquals(RedBlackTreeNode.Color.BLACK, tree.root?.color)
-        assertEquals(RedBlackTreeNode.Color.BLACK, tree.root?.left?.color)
-        assertEquals(RedBlackTreeNode.Color.BLACK, tree.root?.right?.color)
+        assertEquals(3, root.value)
+        assertEquals(1, root.left?.value)
+        assertEquals(4, root.right?.value)
+        assertEquals(RedBlackTreeNode.Color.BLACK, root.color)
+        assertEquals(RedBlackTreeNode.Color.BLACK, root.left?.color)
+        assertEquals(RedBlackTreeNode.Color.BLACK, root.right?.color)
     }
 
 }
