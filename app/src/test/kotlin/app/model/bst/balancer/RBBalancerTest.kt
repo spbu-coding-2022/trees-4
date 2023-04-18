137ffc13034d97d1f5a95f9d5c53fcdcdc09035a
Дьячков Виталий <86203628+YazoonDinalt@users.noreply.github.com>
2023-04-18 23:15:22 +0300

    test: Remove the 'boom', correct the name, correct the ugly brackets

--- app/src/test/kotlin/app/model/bst/balancer/RBBalancerTest.kt
+++ app/src/test/kotlin/app/model/bst/balancer/RBBalancerTest.kt
@@ -1,6 +1,5 @@
 package app.model.bst.balancer
 
-import app.model.bst.RBTree
 import app.model.bst.node.RedBlackTreeNode
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -9,7 +8,7 @@ class RBBalancerTest {
     private val balancer = RBBalancer<Int>()
 
     @Test
-    fun `test method 'add' in rb tree`() {
+    fun `test method 'add' in rb balancer`() {
         var root = balancer.add(null, 1, true)
 
         root = balancer.add(root, 2, true)
@@ -39,8 +38,8 @@ class RBBalancerTest {
     }
 
     @Test
-    fun `test method 'remove' in rb tree`() {
-        var root = balancer.add(null, 1, true)
+    fun `test method 'remove' in rb balancer`() {
+        var root: RedBlackTreeNode<Int>? = balancer.add(null, 1, true)
 
         root = balancer.add(root, 2, true)
         root = balancer.add(root , 3, true)
@@ -48,14 +47,14 @@ class RBBalancerTest {
         /*
                      constructed tree:
                            2(BLACK)
-                         /   \
-                 (BLACK)1     3(BLACK)
-                                \
-                                 4(RED)
+                         /        \
+                    1(BLACK)      3(BLACK)
+                                    \
+                                   4(RED)
                  */
 
 
-        root = balancer.remove(root,2)!!
+        root = balancer.remove(root,2)
 
 
         /*
@@ -66,12 +65,12 @@ class RBBalancerTest {
                  */
 
 
-        assertEquals(3, root.value)
-        assertEquals(1, root.left?.value)
-        assertEquals(4, root.right?.value)
-        assertEquals(RedBlackTreeNode.Color.BLACK, root.color)
-        assertEquals(RedBlackTreeNode.Color.BLACK, root.left?.color)
-        assertEquals(RedBlackTreeNode.Color.BLACK, root.right?.color)
+        assertEquals(3, root?.value)
+        assertEquals(1, root?.left?.value)
+        assertEquals(4, root?.right?.value)
+        assertEquals(RedBlackTreeNode.Color.BLACK, root?.color)
+        assertEquals(RedBlackTreeNode.Color.BLACK, root?.left?.color)
+        assertEquals(RedBlackTreeNode.Color.BLACK, root?.right?.color)
     }
 
 }
