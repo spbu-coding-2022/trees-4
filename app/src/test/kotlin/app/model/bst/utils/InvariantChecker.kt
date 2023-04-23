2b97dcbee7e27b76e742225215df6233e609c3ad
Vadim <88928096+toadharvard@users.noreply.github.com>
2023-04-23 21:29:27 +0300

    fix: Make function description better

--- app/src/test/kotlin/app/model/bst/utils/InvariantChecker.kt
+++ app/src/test/kotlin/app/model/bst/utils/InvariantChecker.kt
@@ -94,13 +94,10 @@ object InvariantChecker {
         }
     }
 
-    /*
-    This function checks that the tree elements
-
-    point to each other correctly. For example, that
-
-    node.left.parent is the same as node
-     */
+    /**
+    * Checks that all nodes linked correctly.
+    * For example, that node.left.parent is the same as node.
+    **/
 
     fun <E : Comparable<E>> isParentLinkedRight(tree: RBTree<E>?): Boolean {
         fun helper(root: RedBlackTreeNode<E>?): Boolean {
