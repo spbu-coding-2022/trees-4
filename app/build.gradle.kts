e4aa05e58d15f31e5d860d5885b7ada491687717
Efim Perevalov <es.perevalov@mail.ru>
2023-05-02 22:16:28 +0300

    refactor: Return the last gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,44 +1,17 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-
-repositories {
-    mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    google()
-}
-
 plugins {
-    kotlin("jvm") version "1.8.10"
-    id("org.jetbrains.compose") version "1.4.0"
-    kotlin("plugin.serialization") version "1.8.10"
+    kotlin("jvm") version "1.8.20"
+    kotlin("plugin.serialization") version "1.8.20"
+    id("io.ktor.plugin") version "2.2.4"
     id("jacoco")
-    id("org.jetbrains.kotlin.plugin.noarg") version "1.8.10"
+    id("org.jetbrains.kotlin.plugin.noarg") version "1.8.20"
+
+    application
 }
-
-dependencies {
-    testImplementation(kotlin("test"))
-    testImplementation(platform("org.junit:junit-bom:5.9.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("org.neo4j:neo4j-ogm-core:4.0.5")
-    runtimeOnly("org.neo4j:neo4j-ogm-bolt-driver:4.0.5")
-    implementation("org.postgresql:postgresql:42.5.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
-    implementation("org.jetbrains.exposed", "exposed-core", "0.40.1")
-    implementation("org.jetbrains.exposed", "exposed-dao", "0.40.1")
-    implementation("org.jetbrains.exposed", "exposed-jdbc", "0.40.1")
-    implementation(compose.desktop.currentOs)
-    implementation(compose.material3)
-    implementation(compose.materialIconsExtended)
-    implementation("com.sksamuel.hoplite:hoplite-core:2.7.2")
-    implementation("com.sksamuel.hoplite:hoplite-json:2.7.4")
-    implementation("com.sksamuel.hoplite:hoplite-yaml:2.7.4")
-
-}
-
 jacoco {
     toolVersion = "0.8.7"
     reportsDirectory.set(layout.buildDirectory.dir("coverage"))
-}
 
+}
 tasks.withType<JacocoReport> {
     reports {
         xml.required.set(true)
@@ -47,26 +20,47 @@ tasks.withType<JacocoReport> {
     }
 }
 
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.neo4j:neo4j-ogm-core:4.0.5")
+    runtimeOnly("org.neo4j:neo4j-ogm-bolt-driver:4.0.5")
+    implementation("org.postgresql:postgresql:42.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation("org.jetbrains.exposed", "exposed-core", "0.40.1")
+    implementation("org.jetbrains.exposed", "exposed-dao", "0.40.1")
+    implementation("org.jetbrains.exposed", "exposed-jdbc", "0.40.1")
+    implementation("com.sksamuel.hoplite:hoplite-core:2.7.2")
+    implementation("com.sksamuel.hoplite:hoplite-json:2.7.4")
+    implementation("com.sksamuel.hoplite:hoplite-yaml:2.7.4")
+
+}
+
 tasks.test {
     useJUnitPlatform()
     finalizedBy(tasks.jacocoTestReport)
 }
 
+tasks.jar {
+    manifest.attributes["Main-Class"] = "app.AppKt"
+}
 noArg {
     annotation("org.neo4j.ogm.annotation.NodeEntity")
     annotation("org.neo4j.ogm.annotation.RelationshipEntity")
 }
 
-compose.desktop {
-    application {
-        mainClass = "app.AppKt"
-
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "trees-visualizer"
-            packageVersion = "1.5.0"
-        }
-    }
+application {
+    // Define the main class for the application.
+    mainClass.set("app.AppKt")
 }
 
-
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
